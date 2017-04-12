f78c48f7c9f82229c3c01935d557481ad8f3f1ef
tommygoggles <tommygoggles@gmail.com>
2017-04-12 22:11:09 +0100

    Add files via upload

---
+++ TGColComp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TGColComp : TGComp {
+
+	// Use this for initialization
+	void Start () {
+        init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        update();
+    }
+
+    new public static void update()
+    {
+        foreach (TGComp thecomp in allthecomps)
+        {
+            Debug.Log(thecomp.gameObject.name);
+        }
+    }
+}
---
+++ TGComp.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class TGComp : MonoBehaviour
+{
+    protected static List<TGComp> allthecomps = new List<TGComp>();
+
+    public static void update()
+    {
+        foreach (TGComp thecomp in allthecomps)
+        {
+            Debug.Log(thecomp.gameObject.name);
+        }
+    }
+
+
+    void Start()//inherrited ones can't call base.start due to private..
+    {
+        init();
+    }
+
+    protected void init () {
+        //add to the compmaster
+        allthecomps.Add(this);
+
+    }
+
+
+    void OnDestroy()//inherrited ones can't call base.
+    {
+        remove();
+    }
+
+    protected void remove()
+    {
+        allthecomps.Remove(this);
+    }
+}
